b82ea18f0b16f50e61cf8a10d230615da82bd1d5
Lucas <developer.lucas@pm.me>
2025-01-09 14:38:22 -0500

    feat: add interval constants and type guards to intervalUtils.ts

---
+++ backend/src/utils/intervalUtils.ts
@@ -0,0 +1,32 @@
+import { PolygonInterval } from "../types/polygon";
+import { YahooInterval } from "../types/yahoo";
+
+// Finer intervals require Yahoo Finance premium
+export const yahooIntervals: YahooInterval[] = [
+  "60m",
+  "1h",
+  "1d",
+  "5d",
+  "1wk",
+  "1mo",
+  "3mo",
+];
+
+export const polygonIntervals: PolygonInterval[] = [
+  "second",
+  "minute",
+  "hour",
+  "day",
+  "week",
+  "month",
+  "quarter",
+  "year",
+];
+
+export const isPolygonInterval = (value: string): value is PolygonInterval => {
+  return polygonIntervals.includes(value as PolygonInterval);
+};
+
+export const isYahooInterval = (value: string): value is YahooInterval => {
+  return yahooIntervals.includes(value as YahooInterval);
+};
