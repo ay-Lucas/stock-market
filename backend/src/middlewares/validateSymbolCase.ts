bb38d18c04eed807990a4de1cf3a06665ff13659
Lucas <developer.lucas@pm.me>
2025-01-10 18:54:30 -0500

    feat: remove symbol redirect to adhere to REST standards and extract symbol middlware logic from validateField to separate file

---
+++ backend/src/middlewares/validateSymbolCase.ts
@@ -0,0 +1,22 @@
+import { NextFunction } from "express";
+import { Request, Response } from "express";
+
+/**
+ * Middleware to ensure the `symbol` parameter is in uppercase.
+ */
+export const validateSymbolCase = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): void => {
+  const { symbol } = req.params;
+
+  const uppercaseSymbol = symbol.toUpperCase();
+
+  // Check if stock symbol is uppercase
+  if (symbol !== uppercaseSymbol) {
+    res.status(400).json({ error: "Symbol stock ticker must be uppercase" });
+    return;
+  }
+  next();
+};
