942fed5432e44f87b4ca8bd142b7bd97523e6b18
Lucas <developer.lucas@pm.me>
2025-01-10 16:29:01 -0500

    feat: add dividend history route

--- backend/src/routes/stockRoutes.ts
+++ backend/src/routes/stockRoutes.ts
@@ -3,6 +3,9 @@ import { validateHistoricalQueryParams } from "../middlewares/validateQueryParam
 import { getStockQuote } from "../controllers/quoteController";
 import { getHistoricalData } from "../controllers/historicalController";
 import { getStockSummary } from "../controllers/summaryController";
+import { getDividendHistory } from "../controllers/dividendController";
+import { validateQueryDates } from "../middlewares/validateQueryDates";
+import { validateSymbolParam } from "../middlewares/validateSymbolParam";
 
 const router = express.Router();
 
@@ -15,4 +18,10 @@ router.get(
 );
 
 router.get("/:symbol/summary", getStockSummary);
+router.get(
+  "/:symbol/dividends",
+  validateSymbolParam,
+  validateQueryDates,
+  getDividendHistory,
+);
 export default router;
