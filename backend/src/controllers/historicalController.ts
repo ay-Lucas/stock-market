7f2424b99bdde940741ac9b0846c198bc417bdc5
Lucas <developer.lucas@pm.me>
2025-01-10 16:53:14 -0500

    refactor: move validate history middleware back to controller

--- backend/src/controllers/historicalController.ts
+++ backend/src/controllers/historicalController.ts
@@ -1,23 +1,52 @@
 import { Request, Response } from "express";
 import { fetchHistoricalData } from "../services/historicalService";
+import {
+  isPolygonInterval,
+  isYahooInterval,
+  polygonIntervals,
+  yahooIntervals,
+} from "../utils/intervalUtils";
 
 export const getHistoricalData = async (
   req: Request,
   res: Response,
 ): Promise<void> => {
   const { symbol } = req.params;
-  const { from, to, interval } = req.query;
+  const { from, to, interval, multiplier } = req.query;
+
+  // Validate `interval`
+  if (
+    !isYahooInterval(interval as string) &&
+    !isPolygonInterval(interval as string)
+  ) {
+    res.status(400).json({
+      error: `Invalid interval. Allowed values are: ${[
+        ...yahooIntervals,
+        ...polygonIntervals,
+      ].join(", ")}`,
+    });
+    return;
+  }
+
+  // Validate multiplier if the interval is a Polygon interval
+  if (isPolygonInterval(interval as string)) {
+    if (!multiplier) {
+      res.status(400).json({
+        error: "Multiplier is required for Polygon intervals",
+      });
+      return;
+    }
+
+    const multiplierNumber = parseInt(multiplier as string);
+    if (isNaN(multiplierNumber) || multiplierNumber <= 0) {
+      res.status(400).json({
+        error: "`multiplier` must be a positive integer",
+      });
+      return;
+    }
+  }
 
   try {
-    if (!symbol) {
-      res.status(400).json({ error: "Symbol parameter is required" });
-    }
-    if (!from || !to || !interval) {
-      res.status(400).json({
-        error: "Query parameters 'from', 'to', and 'interval' are required",
-      });
-    }
-
     const uppercaseSymbol = symbol.toUpperCase(); // Ensure symbol is uppercase
     const historicalData = await fetchHistoricalData(
       uppercaseSymbol,
