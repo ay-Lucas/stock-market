506ec72d05b74cdcc9e05d0779e39c190629c7db
Lucas <developer.lucas@pm.me>
2025-01-11 15:36:59 -0500

    feat: add array of valid YahooScreenerId strings and type guard to utils

---
+++ backend/src/utils/screenerUtils.ts
@@ -0,0 +1,23 @@
+import { YahooScreenerId } from "../types/yahoo";
+
+export const validScrIds: YahooScreenerId[] = [
+  "aggressive_small_caps",
+  "conservative_foreign_funds",
+  "day_gainers",
+  "day_losers",
+  "growth_technology_stocks",
+  "high_yield_bond",
+  "most_actives",
+  "most_shorted_stocks",
+  "portfolio_anchors",
+  "small_cap_gainers",
+  "solid_large_growth_funds",
+  "solid_midcap_growth_funds",
+  "top_mutual_funds",
+  "undervalued_growth_stocks",
+  "undervalued_large_caps",
+];
+
+export const isYahooScreenerId = (value: string): value is YahooScreenerId => {
+  return validScrIds.includes(value as YahooScreenerId);
+};
