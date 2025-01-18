7bfa1967da8f6fd44c3586ace737741d685fd6c1
Lucas <developer.lucas@pm.me>
2025-01-18 15:29:18 -0500

    feat: add chart component using tradingview lightweight charts library

---
+++ frontend/src/components/stocks/Chart.tsx
@@ -0,0 +1,65 @@
+"use client";
+import { ChartComponentProps } from "@/types/chart";
+import { ColorType, createChart, ISeriesApi } from "lightweight-charts";
+import { IChartApi } from "lightweight-charts";
+import { useEffect, useRef } from "react";
+
+export const Chart: React.FC<ChartComponentProps> = ({
+  data,
+  colors: {
+    backgroundColor = "white",
+    lineColor = "#2962FF",
+    textColor = "black",
+    areaTopColor = "#2962FF",
+    areaBottomColor = "rgba(41, 98, 255, 0.28)",
+  } = {},
+}) => {
+  const chartContainerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!chartContainerRef.current) return;
+
+    const chart: IChartApi = createChart(chartContainerRef.current, {
+      layout: {
+        background: { type: ColorType.Solid, color: backgroundColor },
+        textColor,
+        attributionLogo: false,
+      },
+      width: chartContainerRef.current.clientWidth,
+      height: 300,
+    });
+    chart.timeScale().fitContent();
+
+    const newSeries: ISeriesApi<"Area"> = chart.addAreaSeries({
+      lineColor,
+      topColor: areaTopColor,
+      bottomColor: areaBottomColor,
+    });
+    newSeries.setData(data);
+
+    const handleResize = () => {
+      if (chartContainerRef.current) {
+        chart.applyOptions({
+          width: chartContainerRef.current.clientWidth,
+          watermark: { visible: false },
+        });
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+      chart.remove();
+    };
+  }, [
+    data,
+    backgroundColor,
+    lineColor,
+    textColor,
+    areaTopColor,
+    areaBottomColor,
+  ]);
+
+  return <div ref={chartContainerRef} />;
+};
