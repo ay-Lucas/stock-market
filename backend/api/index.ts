3e091dd2cbc65041441acdb6c4455b71615f0b5c
Lucas <developer.lucas@pm.me>
2025-09-05 19:47:07 -0400

    fix(backend): delete unnecessary /api/index.ts

--- backend/api/index.ts
+++
@@ -1,9 +0,0 @@
-import app from "../src/server";
-
-// @ts-ignore
-/* eslint-disable @typescript-eslint/no-explicit-any */
-export default function handler(req: any, res: any) {
-  // Let Express handle the request
-  // @ts-ignore - Express app is compatible with (req, res)
-  return app(req, res);
-}
